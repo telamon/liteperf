0918d32c666ae537d1ca35fd7ac2ebd68c1e7d40
Tony Ivanov <telamohn@gmail.com>
2017-07-11 09:42:31 +0000

    Squashed A-channel

--- samples/basic.rb
+++ samples/basic.rb
@@ -3,10 +3,16 @@ require 'tgfx'
 require 'chunky_png'
 
 img = ChunkyPNG::Image.from_file File.expand_path('../test.png',__FILE__)
+raster= img.pixels.map do |rgba|
+  rgba >> 8
+end
 
 TGfx::FB.init("/dev/fb1") 
 TGfx::FB.clear_screen
-TGfx::FB.draw_image img.pixels, 0,0,img.width
-
+TGfx::FB.draw_image raster, 0,0,img.width
+TGfx::FB.background_color=0x0
+TGfx::FB.foreground_color=0x00ff00
+TGfx::FB.draw_rect 0,0,32,32
+TGfx::FB.foreground_color=0xff0000
 TGfx::FB.draw_text("Hello tgfx/fbgfx",20,10)
 TGfx::FB.sync
