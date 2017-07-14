f309994d1008c4b40d33f4e715e8b947c88264a2
Tony Ivanov <telamohn@gmail.com>
2017-07-15 00:20:37 +0200

    it's alive

--- samples/pngclock.rb
+++ samples/pngclock.rb
@@ -6,6 +6,7 @@ require 'rmagick'
 require 'nokogiri'
 require 'active_support/all'
 begin
+  LightPink = "#ffd0e2"
   width,height=[420,320]
   use_hw = File.exists? '/dev/fb1'
   if use_hw
@@ -14,36 +15,33 @@ begin
     height= TGfx.fb_height
   end
 
-  img = Magick::Image.read(File.expand_path("../clock-bg.png",__FILE__)).first
-  text = Magick::Draw.new
-  text.font_family = 'helvetica'
-  text.gravity = Magick::CenterGravity
-  LightPink = "#ffd0e2"
-  text.pointsize = 48
-  text.fill = LightPink
-  img.annotate text, 165, 48, 280, 95, Time.now.strftime("%H:%M")
-  text.pointsize = 24
-  img.annotate text, 165, 24, 280, 95+48, Time.now.strftime("%A")
+  bg = Magick::Image.read(File.expand_path("../clock-bg.png",__FILE__)).first
+  while true
+	  img = bg.clone  
+	  text = Magick::Draw.new
+	  text.font_family = 'helvetica'
+	  text.gravity = Magick::CenterGravity
+	  text.pointsize = 48
+	  text.fill = LightPink
+	  img.annotate text, 165, 48, 280, 95, Time.now.strftime("%H:%M")
+	  text.pointsize = 24
+	  img.annotate text, 165, 24, 280, 95+48, Time.now.strftime("%A")
 
-  #svg.css("#dow flowPara").first.content= Time.now.wday
-  #svg.css("#monthyear flowPara").first.content = Time.now.strftime("%b %Y")
-  #svg.css("#day flowPara").first.content= Time.now.day
+	  #svg.css("#dow flowPara").first.content= Time.now.wday
+	  #svg.css("#monthyear flowPara").first.content = Time.now.strftime("%b %Y")
+	  #svg.css("#day flowPara").first.content= Time.now.day
 
-  img.resize_to_fill! width, height
-  # Extract the pixel data into RGBA-format compatible with TGfx
-  raster = img.to_blob{ self.format='rgb'}.unpack("C*")
-  # For some reason RMagick outputs duplicate sets of components.
-  # We have to strip them away, seems this is only raspberry/arm related...
-  raster = raster.in_groups_of(6).map do |group|
-     (group[4] | group[2] << 8 | group[0] << 16)
-  end
-
-  if use_hw 
-    TGfx.draw_image raster, 0,0,width
-    TGfx.sync
-  else
-    raster = img.to_blob{ self.format='png'}
-    File.write(File.expand_path("../out.png",__FILE__),raster)
+	  #img.resize_to_fill! width, height
+	  # Extract the pixel data into RGBA-format compatible with TGfx
+	  raster = img.to_blob{ self.format='rgba'}.unpack("L*")
+	  if use_hw 
+	    TGfx.draw_image raster, 0,0,width
+	    TGfx.sync
+	  else
+	    raster = img.to_blob{ self.format='png'}
+	    File.write(File.expand_path("../out.png",__FILE__),raster)
+	  end
+          sleep(5)
   end
 
 rescue Exception => e
